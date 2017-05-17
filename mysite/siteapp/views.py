a57c78b87b55d86b658ac78258e878e886952ea8
McMuffin <hjoseph@unca.edu>
2017-05-17 12:48:44 -0400

    Fixed views.py so it renders Resume.pdf properly

--- mysite/siteapp/views.py
+++ mysite/siteapp/views.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render,render_to_response
+from django.shortcuts import render,render_to_response,HttpResponse
 
 # Create your views here.
 index=lambda request:  render_to_response('index.html')
+
 portfolio = lambda request: render_to_response('portfolio.html')
-resume = lambda request: render_to_response('resume.pdf')
+
+def resume(request): 
+    with open('/home/mcmuffin/testproject/mysite/siteapp/templates/resume.pdf', 'r') as pdf:
+ 	    out=HttpResponse(pdf.read(), content_type='application/pdf')
+ 	    out['Content-Disposition'] = 'filename=HJoseph_Resume.pdf'
+ 	    return out
+    pdf.closed
